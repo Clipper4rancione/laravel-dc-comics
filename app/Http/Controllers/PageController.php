1d908a2a2fbcbd8884c53d79e3094fac375f4a69
Ciro Meluziis <meluziis.ciro99@gmail.com>
2023-01-12 18:38:06 +0100

    [add] comic query

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comic;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index()
     {
-        return view('comics.index');
+        $comics = Comic::all();
+        return view('comics.index', compact('comics'));
     }
 }
