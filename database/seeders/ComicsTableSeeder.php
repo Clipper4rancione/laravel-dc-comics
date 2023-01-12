fb64675ac875f4a5cb4568b3ea4c0baf3a8ec1cc
Ciro Meluziis <meluziis.ciro99@gmail.com>
2023-01-12 17:51:29 +0100

    connected to db in config and launched seeder

--- database/seeders/ComicsTableSeeder.php
+++ database/seeders/ComicsTableSeeder.php
@@ -15,6 +15,19 @@ class ComicsTableSeeder extends Seeder
      */
     public function run()
     {
-        $new_comic = new Comic();
+        $array_comics = config('db');
+
+        foreach ($array_comics as $comic_item) {
+
+            $new_comic = new Comic();
+
+            $new_comic->title = $comic_item['title'];
+            $new_comic->description = $comic_item['description'];
+            $new_comic->image = $comic_item['thumb'];
+            $new_comic->price = $comic_item['price'];
+            $new_comic->series = $comic_item['series'];
+
+            $new_comic->save();
+        }
     }
 }
